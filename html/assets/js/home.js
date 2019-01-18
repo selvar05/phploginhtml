96fe56d88911f5be9c13b1e85cd201b1720af905
R <selva-kumar.a.r@capgemini.com>
2019-01-18 15:02:53 +0530

    added css for approver page

--- html/assets/js/home.js
+++ html/assets/js/home.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
     $(".applicationstatusmap").knob();
+ var popoverHtml =$(this).next(".bs-wizard-info text-center").html()
+    $('.bs-wizard-dot').popover({
+        container:'.bs-wizard',
+         html : true,
+        content:function() {
+            return $(this).next(".bs-wizard-info").find(".details").html()
+        },
+        boundary:"scrollParent",
+        placement:'bottom',
+        fallbackPlacement:'flip',
+        title:function() {
+            return $(this).next(".bs-wizard-info").find(".title-txt").html()
+        },
+       
+        })
+      $('.bs-wizard-dot').popover('show')
 })
